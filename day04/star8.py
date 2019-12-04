9704e6ee192cd51ee35fe22252a419acf1ed57d7
jenxuzear <jennifexu@gmail.com>
2019-12-04 07:53:05 -0800

    Fixed some bugs. star8.py now works.

--- day04/star8.py
+++ day04/star8.py
@@ -7,16 +7,21 @@ def main():
 		digits_list = np.array([int(d) for d in str(test_num)])
 		digits_diff = np.diff(digits_list)
 		adjacent_bools = digits_diff==0
-		np.insert(adjacent_bools, 0, 0)
-		np.append(adjacent_bools, 0)
+		adjacent_bools = np.insert(adjacent_bools, 0, 0)
+		adjacent_bools = np.append(adjacent_bools, 0)
 		searchval = [0,1,0]
 		possibles = np.where(adjacent_bools==searchval[0])[0]
 		if (digits_diff>=0).all():
+			# print("test_num = ", test_num)  #DELME
+			# print("digits_dff = ", digits_diff)  #DELME
+			# print("adjacent_bools = ", adjacent_bools)  #DELME
+			# print("possibles = ", possibles)  #DELME
 			for p in possibles:
 				check = adjacent_bools[p:p+3]
-				if (check==searchval).all():
+				# print("check = ", check)  #DELME
+				if np.all(check==searchval):
 					success_counter += 1
-					continue
+					break
 	print("Number of successes=", success_counter)
 
 
