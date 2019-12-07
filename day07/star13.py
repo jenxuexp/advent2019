3b8fec3264a73a3ea50ce09bc0c66da3e734deed
jslazear <jlazear@gmail.com>
2019-12-07 14:03:52 -0500

    Somehow lost star 10 and messed up the ordering... fixing

--- day07/star13.py
+++ day07/star13.py
@@ -4,39 +4,23 @@ import numpy as np
 from intcode import IntcodeComputer
 
 
-possibilities = permutations([5, 6, 7, 8, 9])
-fname = 'input.txt'
-
-# possibilities = [[9,8,7,6,5]]
-# fname = 'input_test1.txt'
-
-# possibilities = [[9,7,8,5,6]]
-# fname = 'input_test2.txt'
-
+possibilities = permutations([0, 1, 2, 3, 4])
 
 outputs = []
 for phases in possibilities:
 	ph_A, ph_B, ph_C, ph_D, ph_E = phases
-	amp_A = IntcodeComputer(fname, allow_pausing=True, name='amp_A')
-	amp_B = IntcodeComputer(fname, allow_pausing=True, name='amp_B')
-	amp_C = IntcodeComputer(fname, allow_pausing=True, name='amp_C')
-	amp_D = IntcodeComputer(fname, allow_pausing=True, name='amp_D')
-	amp_E = IntcodeComputer(fname, allow_pausing=True, name='amp_E')
+	amp_A = IntcodeComputer('input.txt')
+	amp_B = IntcodeComputer('input.txt')
+	amp_C = IntcodeComputer('input.txt')
+	amp_D = IntcodeComputer('input.txt')
+	amp_E = IntcodeComputer('input.txt')
 
 	amp_A.run([ph_A, 0])
-	amp_B.run([ph_B, amp_A.output[-1]])
-	amp_C.run([ph_C, amp_B.output[-1]])
-	amp_D.run([ph_D, amp_C.output[-1]])
-	amp_E.run([ph_E, amp_D.output[-1]])
+	amp_B.run([ph_B, amp_A.output[0]])
+	amp_C.run([ph_C, amp_B.output[0]])
+	amp_D.run([ph_D, amp_C.output[0]])
+	amp_E.run([ph_E, amp_D.output[0]])
 
-	while amp_E.continue_flag:
-		amp_A.resume([amp_E.output[-1]])
-		amp_B.resume([amp_A.output[-1]])
-		amp_C.resume([amp_B.output[-1]])
-		amp_D.resume([amp_C.output[-1]])
-		amp_E.resume([amp_D.output[-1]])
-
-	outputs.append(amp_E.output[-1])
+	outputs.append(amp_E.output[0])
 
 print(max(outputs))
-
