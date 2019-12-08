7fe0807ed1ee44bac7d79ac8e77b63d8427f9a1c
jenxuzear <jennifexu@gmail.com>
2019-12-07 22:07:53 -0800

    finished star16

--- day08/star16.py
+++ day08/star16.py
@@ -1,4 +1,5 @@
 import numpy as np
+from matplotlib.pyplot import *
 
 width = 25
 height = 6
@@ -10,3 +11,13 @@ with open('input.txt') as f:
 data = np.array([int(x) for x in data])
 data = data.reshape(-1, height, width)
 
+image = np.zeros([height, width])
+for i in range(data.shape[1]):
+	for j in range(data.shape[2]):
+		stack = data[:, i, j]
+		imin = np.where(stack != 2)[0][0]
+		val = stack[imin]
+		image[i, j] = val
+
+imshow(image)
+show()
